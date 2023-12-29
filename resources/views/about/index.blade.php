0b82fedb8c918ae9083199a1be335f4a6d44dd05
unknown <dhuhacmd@gmail.com>
2023-12-29 12:35:43 +0700

    first

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,62 @@
+<!-- ======= About Us Section ======= -->
+<section id="about">
+    <div class="container" data-aos="fade-up">
+
+        <header class="section-header">
+            <h3>{{ __('About Us') }}</h3>
+            <p>
+            </p>
+        </header>
+
+        <div class="row about-cols">
+
+            <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
+                <div class="about-col">
+                    <div class="img">
+                        <img src="assets/img/about-mission.jpg" alt="" class="img-fluid" >
+                        <div class="icon"><i class="bi bi-bar-chart"></i></div>
+                    </div>
+                    <h2 class="title"><a href="#">{{ __('Our Mission') }}</a></h2>
+                    <p style="text-align: justify;
+                    text-justify: inter-word;">
+                        Menjadi penopang yang kokoh untuk bertumbuhnya suatu karya dan
+                        berfokus dalam dunia entertainment khususnya pada produksi ide audio dan visual. 
+                        Serta bergerak untuk membangun ekosistem dalam industri kreatif
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
+                <div class="about-col">
+                    <div class="img">
+                        <img src="assets/img/about-plan.jpg" alt="" class="img-fluid">
+                        <div class="icon"><i class="bi bi-brightness-high"></i></div>
+                    </div>
+                    <h2 class="title"><a href="#">Our Plan</a></h2>
+                    <p style="text-align: justify;
+                    text-justify: inter-word;">
+                        Senantiasa memberikan ide dan inovasi  baru untuk bersaing dalam industri global
+                        serta menjadi perusahaan yang kompetitif, inovatif, dan selalu menjadi pioner dalam ide-ide kreatif yang kami lahirkan.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-md-4" data-aos="fade-up" data-aos-delay="300">
+                <div class="about-col">
+                    <div class="img">
+                        <img src="assets/img/about-vision.jpg" alt="" class="img-fluid">
+                        <div class="icon"><i class="bi bi-calendar4-week"></i></div>
+                    </div>
+                    <h2 class="title"><a href="#">{{ __('Our Vision') }}</a></h2>
+                    <p style="text-align: justify;
+                    text-justify: inter-word;">
+                        Sebagai Wadah bagi talenta muda berbakat yang ingin menekuni dunia entertainment.
+                        Sebagai sarana edukasi mengenai industri seni dan hiburan.  
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section><!-- End About Us Section -->
---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,55 @@
+<!-- ======= Our Clients Section ======= -->
+<section id="clients">
+    <div class="container" data-aos="zoom-in">
+
+        <header class="section-header">
+            <h3>Our Clients</h3>
+        </header>
+
+        <div class="clients-slider swiper">
+            <div class="swiper-wrapper align-items-center">
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/pertamina-logo.png') }}" class="img-fluid"
+                        alt="" style="max-width: 70%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/pp-logo.png') }}" class="img-fluid"
+                        alt="" style="max-width: 50%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/pm-logo.png') }}" class="img-fluid"
+                        alt="" style="max-width: 50%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/dipar-logo.png') }}" class="img-fluid"
+                        alt="" style="max-width: 80%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/permikomnas-logo.png') }}"
+                        class="img-fluid" alt="" style="max-width: 50%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/umm-logo.png') }}" class="img-fluid"
+                        alt="" style="max-width: 80%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/kemenpar-logo.png') }}"
+                        class="img-fluid" alt="" style="max-width: 50%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/hp-record.png') }}" class="img-fluid"
+                        alt="" style="max-width: 50%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/hrprod-logo.jpg') }}" class="img-fluid"
+                        alt="" style="max-width: 50%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/brianlogo.jpg') }}" class="img-fluid"
+                        alt=""></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/ledrelogo.jpg') }}" class="img-fluid"
+                        alt=""></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/jurnabalogo.png') }}"
+                        class="img-fluid" alt=""></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/frontmanlogo.png') }}"
+                        class="img-fluid" alt="" style="max-width: 60%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/ndalemleremlogo.jpg') }}"
+                        class="img-fluid" alt="" style="max-width: 50%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/ceritaleremlogo.png') }}"
+                        class="img-fluid" alt=""></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/kopitanilogo.jpg') }}"
+                        class="img-fluid" alt="" style="max-width: 60%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/drembanlogo.png') }}"
+                        class="img-fluid" alt=""></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/cmdlogo.png') }}" class="img-fluid"
+                        alt="" style="max-width: 50%;"></div>
+                <div class="swiper-slide"><img src="{{ asset('assets/img/clients/bch-logo.jpg') }}" class="img-fluid"
+                        alt="" style="max-width: 50%;"></div>
+
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+
+    </div>
+</section><!-- End Our Clients Section -->
